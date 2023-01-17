604eaca26fb4ae54701429ad755b6d1d40478f98
Rakou <92184755+Rakou-FR@users.noreply.github.com>
2023-01-17 15:42:38 +0100

    Add files via upload

--- ligne.sh
+++ ligne.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # on définit les variables pour les commandes à exécuter 
-command1="ls -l"
-command2="df -h"
+command1='grep -n "Bénéfice" source_html.txt'
+command2='grep -n "Bénéfice" source_html.txt'
 
 # on crée le fichier de sortie 
-output="command_output.txt"
+output="output.txt"
 
 # on exécute les deux commandes et on stocke le résultat dans le fichier de sortie
 $command1 >> $output
