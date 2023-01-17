fbe8638d6c02ec87c71140c2fb285fc227461307
Rakou <92184755+Rakou-FR@users.noreply.github.com>
2023-01-17 15:07:55 +0100

    Add files via upload

--- scrapus.py
+++ scrapus.py
@@ -1,3 +1,19 @@
-import os
-url = 'https://www.someurl.com'
-os.system(f"""wget -n "{url}""")
+# Importer les bibliothèques nécessaires
+import requests
+from bs4 import BeautifulSoup
+ 
+# Définir l'URL du site web
+url = "https://www.boursorama.com/cours/1rPSWP/"
+ 
+# Récupérer le code source HTML
+r = requests.get(url)
+ 
+# Créer un objet BeautifulSoup
+soup = BeautifulSoup(r.content, "html.parser")
+ 
+# Obtenir le code source HTML
+html = soup.prettify()
+ 
+# Sauvegarder le code source HTML dans un fichier
+with open("source_html.txt", "w", encoding="utf-8") as f:
+    f.write(html)
