89c546c8a9fda787fb2a67c42fa1f6724aed54e5
Rakou <92184755+Rakou-FR@users.noreply.github.com>
2023-01-18 10:42:16 +0100

    Add files via upload

--- scrapus.py
+++ scrapus.py
@@ -2,8 +2,22 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import os
  
+
+# 57
+'''
+1 : dl la page
+2 : cherche dedans numéro ligne &> txt
+3 : nettoie fichier txt &> new.txt
+4 : découpe la page de [a ; a+57] &> txt_1
+5 : nettoie fichier txt_1 &> new.txt_1
+'''
 class monde:
+
+    def bash_start():
+        os.system("./ligne.sh")
+        
     def get():
         # Définir l'URL du site web
         url = "https://www.boursorama.com/cours/1rPSWP/"
@@ -21,25 +35,45 @@ class monde:
         with open("source_html.txt", "w", encoding="utf-8") as f:
             f.write(html)
 
+    def clean(a):
+                # ouverture du fichier et lecture des lignes
+        with open(a, "r") as fichier:
+            lines = fichier.readlines()
 
-    def decoupe(a,b):
+        # ecriture dans un nouveau fichier
+        with open("fichier_nouveau.txt", "w") as fichier_nouveau:
+            # parcours chaque ligne du fichier
+            for line in lines:
+                # parcours chaque mot de la ligne
+                for word in line.split():
+                    # test si le mot est un int
+                    if word.isdigit():
+                        # ecriture dans le nouveau fichier
+                        fichier_nouveau.write(word + " ")
+
+        # fermeture du fichier
+        fichier.close()
+
+    def decoupe():
         #on commence par importer le module sys 
 
         # on définit les variables pour les lignes de départ et d'arrivée 
         start_line = int(a)
-        end_line = int(b)
+        end_line = int(a+57)
 
         # on ouvre le fichier et le ligne par ligne 
-        with open("sample_file.txt", "r") as f:
+        with open("source_html.txt", "r") as f:
             lignes = f.readlines()
 
         # on découpe le fichier entre les lignes de départ et d'arrivée 
         decoupe_fichier = lignes[start_line:end_line]
 
         # on écrit le nouveau fichier 
-        with open("new_file.txt", "w", encoding="utf-8") as f:
+        with open("limited.txt", "w", encoding="utf-8") as f:
             for ligne in decoupe_fichier:
                 f.write(ligne)
 
+
 monde.get()
-monde.decoupe(5579,5600)
+monde.bash_start()
+monde.clean("limited.txt")
