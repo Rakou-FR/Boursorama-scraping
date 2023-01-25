7e59524fd45934326e02b9c1a7f4ded6b2eb05a8
Rakou <balint.kis@outlook.fr>
2023-01-25 10:57:48 +0100

    V1.005
    V1.005 Class cours working

--- scrapus.py
+++ scrapus.py
@@ -150,58 +150,77 @@ class cours:
         liste = []
         # ouverture 6
         liste_1 = cours.clean(cours.find("ouverture"),6)
-        for i in range(0, len(liste_1), len(liste_1)):
-            liste.append(str(liste_1[i]) + str(liste_1[i+1]) + str(liste_1[i+2])
-            + str(liste_1[i+3]) + str(liste_1[i+4] + str(liste_1[i+5])))
+        liste.append("ouverture :")
+        for item in liste_1:
+            liste[len(liste)-1] += str(item)
         
         # cloture 6
         liste_2 = cours.clean(cours.find("clôture veille"),6)
-        for i in range(0, len(liste_2), len(liste_2)):
-            liste.append(str(liste_2[i]) + str(liste_2[i+1]) + str(liste_2[i+2])
-            + str(liste_2[i+3]) + str(liste_2[i+4] + str(liste_2[i+5])))
+        liste.append("cloture :")
+        for item in liste_2:
+            liste[len(liste)-1] += str(item)
 
         # + haut 6
         liste_3 = cours.clean(cours.find("+ haut"),6)
-        for i in range(0, len(liste_3), len(liste_3)):
-            liste.append(str(liste_3[i]) + str(liste_3[i+1]) + str(liste_3[i+2])
-            + str(liste_3[i+3]) + str(liste_3[i+4] + str(liste_3[i+5])))
+        liste.append("+ haut : ")
+        for item in liste_3:
+            liste[len(liste)-1] += str(item)
 
         # + bas 6
         liste_4 = cours.clean(cours.find("+ bas"),6)
-        for i in range(0, len(liste_4), len(liste_4)):
-            liste.append(str(liste_4[i]) + str(liste_4[i+1]) + str(liste_4[i+2])
-            + str(liste_4[i+3]) + str(liste_4[i+4] + str(liste_4[i+5])))
+        liste.append("+ bas : ")
+        for item in liste_4:
+            liste[len(liste)-1] += str(item)
+
         # volume 6
+        liste_5 = cours.clean(cours.find("capital échangé")-10,3)
+        liste.append("volume : ")
+        for item in liste_5:
+            liste[len(liste)-1] += str(item)
+        
         # capital 6
+        liste_6 = cours.clean(cours.find("capital échangé"),6)
+        liste.append("capital : ")
+        for item in liste_6:
+            liste[len(liste)-1] += str(item)
+
         # valorisation 6
-        # dernier 6
-        # limite + et - 51
+        liste_7 = cours.clean(cours.find("valorisation"),6)
+        liste.append("valorisation : ")
+        for i in range(0,len(liste_7)):
+            liste[len(liste)-1] += str(liste_7[i])
+
+        # LIMITE À LA HAUSSE
+        liste_8_1 = cours.clean(cours.find("limite à la baisse")+47,7)
+        liste.append("lim hausse : ")
+        for i in range(0,len(liste_8_1)):
+            liste[len(liste)-1] += str(liste_8_1[i])
+
+        # LIMITE À LA BAISSE
+        liste_8 = cours.clean(cours.find("limite à la baisse")+15,7)
+        liste.append("lim baisse : ")
+        for i in range(0,len(liste_8)):
+            liste[len(liste)-1] += str(liste_8[i])
+
         # rendemet 6
+        liste_9 = cours.clean(cours.find("rendement estimé"),6)
+        liste.append("rendement : ")
+        for i in range(0,len(liste_9)):
+            liste[len(liste)-1] += str(liste_9[i])
+
         # PER estimé 27
+        liste_10 = cours.clean(cours.find("PER estimé")+10,17)
+        liste.append("PER : ")
+        for i in range(0,len(liste_10)):
+            liste[len(liste)-1] += str(liste_10[i])
+
         # dividende et date dividente 16
+        liste_11 = cours.clean(cours.find("dernier dividende"),6)
+        liste.append("dividende : ")
+        for i in range(0,len(liste_11)):
+            liste[len(liste)-1] += str(liste_11[i])
+
         return liste
-    
-        
-        '''output_list = []
-        
-        for i in range(0, 24, 6):
-            output_list.append(str(liste[i]) + str(liste[i+1]) + str(liste[i+2])
-            + str(liste[i+5]) + str(liste[i+4]))
-
-        for i in range(len(liste), 27, 3):
-            output_list.append(str(liste[i]) + str(liste[i+1]) + str(liste[i+2]))
-
-        for i in range(27, len(liste), 4):
-            output_list.append(str(liste[i]) + str(liste[i+1]) + str(liste[i+2]) 
-            + str(liste[i+3]))
-
-        final = [
-            int(output_list[0])/100,int(output_list[1])/100,int(output_list[2])/100,
-            int(output_list[3])/100,int(output_list[4])/100,int(output_list[5])/100,
-            int(output_list[6])/100,int(output_list[7])/100,int(output_list[8])/100,
-            int(output_list[9])/100,int(output_list[10])/100,int(output_list[11])/100
-        ]
-        return final, "evo"'''
 
 
 if __name__ == '__main__':
